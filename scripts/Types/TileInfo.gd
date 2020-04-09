936256b90cb59d23fe35ed4b8cb8f3f5d946bcd4
River Gillis <jrivergillis@gmail.com>
2020-04-08 21:24:17 -0500

    Display tiles with worlgen

--- scripts/Types/TileInfo.gd
+++ scripts/Types/TileInfo.gd
@@ -7,5 +7,9 @@ var x: int = 0
 var y: int = 0
 # TODO: Coordinates in world-tile space
 
+func _init(x: int = 0, y: int = 0) -> void:
+	self.x = x
+	self.y = y
+
 func _to_string() -> String:
 	return "TileInfo(%d, %d)" % [x, y]
---
+++ scripts/WorldGen.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+class_name WorldGen
+
+const TileBG = preload("res://scenes/TileBG.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for y_tile in range(1, Constants.SCREEN_TILES_VERT + 1):
+		for x_tile in range(1, Constants.SCREEN_TILES_HORIZ + 1):
+			var tile : TileInfo = TileInfo.new(x_tile, y_tile)
+			var tile_bg = TileBG.instance()
+			tile_bg.position = Utils.screen_tile_to_px(tile)
+			add_child(tile_bg)
